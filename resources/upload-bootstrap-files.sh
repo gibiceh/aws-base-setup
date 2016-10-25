96d9101025257bd7b084cbfe70b9a5995f9e6133
Geoffrey Hickman <geoff.hickmanii@gmail.com>
2016-10-25 15:21:30 -0500

    cleaned up upload-bootstrap-files.sh

--- resources/upload-bootstrap-files.sh
+++ resources/upload-bootstrap-files.sh
@@ -22,27 +22,17 @@ function build_custom_resource() {
   zip -r $CURRENT_DIR/$1.zip .
 }
 
->>>>>>> 9b7d460039404db119ef7086991afccd58f2ce73
 function main() {
   CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   source "$CURRENT_DIR/../settings.sh"
   source "$CURRENT_DIR/../utilities.sh"
 
-<<<<<<< HEAD
-  aws --profile $settings_aws_cli_profile \
-    s3 sync $CURRENT_DIR s3://$settings_prelim_s3repos_resources_bucketname \
-    --exclude "upload-bootstrap-files.sh" \
-=======
-  build_custom_resource "custom-resources"
   build_custom_resource "amilookup"
 
   aws --profile $settings_aws_cli_profile \
     s3 sync $CURRENT_DIR s3://$settings_prelim_s3repos_resources_bucketname \
     --exclude "upload-bootstrap-files.sh" \
-    --exclude "custom-resources/*" \
     --exclude "amilookup/*" \
-    --exclude "flamenco/instances/*" \
->>>>>>> 9b7d460039404db119ef7086991afccd58f2ce73
     --delete \
     || { echo "Could not upload resource files to s3://$settings_prelim_s3repos_resources_bucketname. Does the bucket exist?"; exit 1;}
 }
